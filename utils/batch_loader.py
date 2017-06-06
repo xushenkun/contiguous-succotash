80ae01d2ef150dd7788b7a7955a3822c8ff6530e
xushenkun <xsk1983@126.com>
2017-06-06 16:26:44 +0800

    split chinese corpus lines

--- utils/batch_loader.py
+++ utils/batch_loader.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-
+
 import collections
 import os
 import re
@@ -342,3 +344,36 @@ class BatchLoader:
     def decode_characters(self, characters_idx):
         characters = [self.idx_to_char[i] for i in characters_idx]
         return ''.join(characters)
+
+
+def split_corpus_line(line, max_seq_len=200, seg_char=u'。', seg_len=4):
+    lines = []
+    if len(line) <= max_seq_len:
+        return line
+    else:
+        segs = line.split(seg_char)
+        cur = []
+        for i,seg in enumerate(segs):            
+            if i!=0 and i%seg_len==0:
+                if len(cur) > 0:
+                    lines.append(cur)
+                cur =[]
+            cur.append(seg)
+        length = len(lines)
+        if length >= 2:
+            if len(lines[-1]) < seg_len:
+                lines[-2] = lines[-2] + lines[-1]
+                lines = lines[0:-1]
+        return '\n'.join([u'。'.join(line)+u'。' for line in lines])
+
+if __name__ == '__main__':
+    import codecs
+    filename='poem_test.txt'
+    with codecs.open('../data/'+filename, "r", encoding="utf-8") as fi:
+        data = fi.read()
+    lines = data.split('\n')
+    new_lines = []
+    for line in lines:
+        new_lines.append(split_corpus_line(line))
+    with codecs.open('../data/'+filename+'.ok', "w", encoding="utf-8") as fo:
+        fo.write('\n'.join(new_lines))
